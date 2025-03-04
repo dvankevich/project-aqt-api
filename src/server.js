f3687c1fa869fca204db779e51fa913fc78d2d89
dvankevich <dvankevich@gmail.com>
2025-03-04 18:37:27 +0200

    added middlewares

--- src/server.js
+++ src/server.js
@@ -4,12 +4,19 @@ import cors from 'cors';
 
 import { getEnvVar } from './utils/getEnvVar.js';
 
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
 
-  app.use(express.json());
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+    }),
+  );
   app.use(cors());
 
   app.use(
@@ -26,18 +33,9 @@ export const startServer = () => {
     });
   });
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
