14e5285c6432160c8955bb13aee7c3bc83116cae
dvankevich <dvankevich@gmail.com>
2025-03-05 10:34:15 +0200

    added usersSchema

--- src/db/models /user.js
+++ src/db/models /user.js
@@ -1,11 +1,27 @@
-//import { model, Schema } from 'mongoose';
+import { model, Schema } from 'mongoose';
 
-// (ім'я,emailу, статі, ваги, активного часу на спорт, денної норми води та аватарки)
+const usersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    gender: { type: String, enum: ['woman', 'man'], default: 'woman' },
+    weight: { type: Number, default: 0 },
+    dailySportTime: { type: Number, default: 0 },
+    dailyNorm: { type: Number, default: 1500 },
+    avatarUrl: {
+      type: String,
+      default:
+        'https://res.cloudinary.com/dvc0lg6q7/image/upload/v1741163238/person_qyhqpa.png',
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
 
-// name String min 2 max 12
-// email String +
-// gender [woman, man] default woman
-// weight кг. default 65
-// dailySportTime
-// dailyWaterIntake default 1.5l зберігати в мл.
-// avatarUrl
+usersSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export const UsersCollection = model('users', usersSchema);
