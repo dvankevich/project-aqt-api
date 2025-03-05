9d523ef12283c15765397663113dccfdf3b36acf
dvankevich <dvankevich@gmail.com>
2025-03-05 19:32:32 +0200

    added login endpoint

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,6 +4,7 @@ import {
   resetPassword,
 } from '../services/auth.js';
 import { loginUser } from '../services/auth.js';
+import { ONE_DAY } from '../constants/index.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -16,9 +17,24 @@ export const registerUserController = async (req, res) => {
 };
 
 export const loginUserController = async (req, res) => {
-  await loginUser(req.body);
+  const session = await loginUser(req.body);
 
-  // далі ми доповнемо цей контролер
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+
+  res.json({
+    status: 200,
+    message: 'Successfully logged in an user!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
 };
 
 export const requestResetEmailController = async (req, res) => {
