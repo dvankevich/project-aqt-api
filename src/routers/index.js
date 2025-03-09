1183611eb991dcedad80dc81be392a85afeedd3b
vanok00 <vanok008@ukr.net>
2025-03-09 22:06:07 +0200

    changed-registered-users-router

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,14 +2,11 @@ import { Router } from 'express';
 import usersRouter from './user.js';
 import authRouter from './auth.js';
 import waterRouter from './water.js';
-import statsRoutes from './stats.js';
 
 const router = Router();
 
 router.use('/users', usersRouter);
 router.use('/water', waterRouter);
-router.use('/stats', statsRoutes);
-
 router.use('/auth', authRouter);
 
 export default router;
