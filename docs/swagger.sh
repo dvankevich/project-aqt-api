34d064633b42c2729e75a3aa00621df8c2a63c46
dvankevich <dvankevich@gmail.com>
2025-03-16 13:41:01 +0200

    add openapi schema

--- docs/swagger.sh
+++ docs/swagger.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 jq 'walk(if type == "object" and has("header") then del(.header) else . end)' AquaTrackAPI.postman_collection.json > postman.json
 postman2openapi postman.json > openapi.yaml
+sed -i 's/openapi: 3\.0\.3/openapi: 3.1.0/g' openapi.yaml
+sed -i '/title: AquaTrackAPI/r license.yaml' openapi.yaml
+sed -i '/components:/r schemas.yaml' openapi.yaml
 npm run build-docs
+
